bec1f3438154d15ec96aa0b21f002f5122d0ee47
ddkyumaprogrammer <ddkyuma.programmer@gmail.com>
2019-08-08 14:05:39 +0430

    local settings

--- mahimo/local_settings.py
+++ mahimo/local_settings.py
@@ -1,6 +1,3 @@
-import os
-from mahimo import settings
-
 
 # django-allauth Configuration variables you might like to change.
 
@@ -16,10 +13,11 @@ ACCOUNT_AUTHENTICATION_METHOD = "username_email"
 # Determines the expiration date of email confirmation mails (# of days).
 ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
 
-# ACCOUNT_EMAIL_CONFIRMATION_HMAC =True
+
+ACCOUNT_EMAIL_CONFIRMATION_HMAC = False
 
 # The user is required to hand over an e-mail address when signing up.
-ACCOUNT_EMAIL_REQUIRED = False
+ACCOUNT_EMAIL_REQUIRED = True
 
 # Determines the e-mail verification method during signup. When set to
 # "mandatory" the user is blocked from logging in until the email
@@ -27,13 +25,13 @@ ACCOUNT_EMAIL_REQUIRED = False
 # with an unverified e-mail address. In case of "optional", the e-mail
 # verification mail is still sent, whereas in case of "none" no e-mail
 # verification mails are sent.
-ACCOUNT_EMAIL_VERIFICATION = "optional"
-# ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+# ACCOUNT_EMAIL_VERIFICATION = "optional"
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+
 
 # Subject-line prefix to use for email messages sent. By default, the name
 # of the current Site (django.contrib.sites) is used.
-ACCOUNT_EMAIL_SUBJECT_PREFIX = '[Site] '
-
+ACCOUNT_EMAIL_SUBJECT_PREFIX = ' mahimo.com '
 # A string pointing to a custom form class
 # (e.g. 'myapp.forms.SignupForm') that is used during signup to ask
 # the user for additional input (e.g. newsletter signup, birth
@@ -78,7 +76,28 @@ SOCIALACCOUNT_AUTO_SIGNUP = True
 # 'avatar' in settings.INSTALLED_APPS.
 # SOCIALACCOUNT_AVATAR_SUPPORT
 
+ACCOUNT_EMAIL_MAX_LENGTH =254
+# Maximum length of the email field.
+# You won’t need to alter this unless using MySQL with
+# the InnoDB storage engine and the utf8mb4 charset,
+# and only in versions lower than 5.7.7,
+# because the default InnoDB settings don’t allow indexes bigger
+# than 767 bytes. When using utf8mb4, characters are 4-bytes wide,
+# so at maximum column indexes can be 191 characters long (767/4).
+# Unfortunately Django doesn’t allow specifying index lengths,
+# so the solution is to reduce the length in characters of indexed
+# text fields. More information can be found at MySQL’s documentation
+# on converting between 3-byte and 4-byte Unicode character sets.
+
 ACCOUNT_LOGOUT_ON_GET = True
+# Determines whether or not the user is automatically logged out
+# by a GET request. GET is not designed to modify the server state,
+# and in this case it can be dangerous.
+# See LogoutView in the documentation for details.
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+# When signing up, let the user type in their email address twice to avoid typo’s.
+
+REST_USE_JWT = True
 
 REST_SESSION_LOGIN = True
 # Dictionary containing provider specific settings.
